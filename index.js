c46ee88071f751d46918bef2a66dc494a53f29ad
kevin diesenberg <kdiesenb@gmail.com>
2022-10-31 13:22:43 -0400

    commit tree 43f34d49ad0d2aad0f29d6c1c8df61498ddb5124

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = "bob";
+var upperCaseCustomerName = function() {
+    customerName = customerName.toUpperCase();
+}
+
+var setBestCustomer = function() {
+   bestCustomer = "not bob";
+}
+
+var overwriteBestCustomer = function() {
+    bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "billy";
+
+var changeLeastFavoriteCustomer =function() {
+    leastFavoriteCustomer = 'rov'
+}
